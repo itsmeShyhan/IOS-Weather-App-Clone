61b491f73ba3bac4bd0be1687b58859d8f42b3fe
itsmeShyhan <127602434+itsmeShyhan@users.noreply.github.com>
2025-03-21 02:29:38 +0530

    Added all the project files

---
+++ CWKTemplate24/Model/AirDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  AirDataModel.swift
+//  CWKTemplate24
+//
+//  Created by girish lukka on 23/10/2024.
+//
+
+import Foundation
+
+/* Code for AirDataModel Struct */
+
+struct AirDataModel: Codable{
+
+    // MARK:  list of attributes to map response from openweather pollution api
+}
